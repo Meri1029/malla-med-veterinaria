ee8531dbac47173c047c8b5846d5cac76970e23b
Meri1029 <mariagalanrojas123@gmail.com>
2025-07-15 15:31:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -15,12 +15,255 @@ let asignaturas = {
     descripcion: '',
     requisito: 'bioestadistica'
   },
-  'micro': {
-    nombre: 'Microbiología',
+  'zoologia': {
+    nombre: 'Zoología',
     descripcion: '',
-    requisito: 'bioquimica'
-  }
-};
+    requisito: 'ecología'
+  },
+  'OMV': {
+    nombre: 'OMV',
+    descripcion: '',
+    requisito: null
+     },
+  'ingles beginner': {
+    nombre: 'Ingles beginner',
+    descripcion: '',
+    requisito: 'ingles preintermedio'
+      },
+
+  // 🔸 Semestre 2
+  'bases moleculares y celulares': {
+    nombre: 'bases moleculares y celulares',
+    descripcion: '',
+    requisito: 'fisiologia I', 'bases inmunologicas', 'agentes biologicos patógenos'
+       },
+  'bases celulares': {
+    nombre: 'bases celulares',
+    descripcion: '',
+    requisito: 'fisiologia I', 'agentes biologicos patógenos'
+       },
+  'bioestadistica': {
+    nombre: 'bioestadistica',
+    descripcion: '',
+    requisito: 'epidemiologica', 'MAIG'
+       },
+  'est. macro del organismo animal': {
+    nombre: 'est. macro del organismo animal',
+    descripcion: '',
+    requisito: 'fisiologia II'
+       },
+  'des. micro del organismo animal': {
+    nombre: 'des. micro del organismo animal',
+    descripcion: '',
+    requisito: null
+       },
+  'ecologia': {
+    nombre: 'ecologia',
+    descripcion: '',
+    requisito: 'conducta animal', 'MAEP'
+       },
+  'seminario espacio A': {
+    nombre: 'seminario espacio A',
+    descripcion: '',
+    requisito: null
+       },
+  'practica general básica': {
+    nombre: 'practica general básica',
+    descripcion: '',
+    requisito: null
+       },
+  'ingles preintermedio': {
+    nombre: 'ingles preintermedio',
+    descripcion: '',
+    requisito: null
+ },
+   
+     // 🔸 Semestre 3
+  'est. macro. del organismo animal': {
+    nombre: 'est. macro. del organismo animal',
+    descripcion: '',
+    requisito: 'fisiologia II'
+       },
+  'fisiologia I': {
+    nombre: 'fisiologia I',
+    descripcion: '',
+    requisito: 'fisiologia II', 'farmacologia general'
+       },
+  'epidemiologia ': {
+    nombre: 'epidemiologia',
+    descripcion: '',
+    requisito: 'enf. infecciosas y parasitarias'
+       },
+  'bases economicas': {
+    nombre: 'bases economicas',
+    descripcion: '',
+    requisito: 'MAPLAN'
+       },
+  'des. micro del organismo animal': {
+    nombre: 'des. micro del organismo animal',
+    descripcion: '',
+    requisito: null
+       },
+  'conducta animal': {
+    nombre: 'conducta animal',
+    descripcion: '',
+    requisito: 'MACA'
+       },
+  'practica clinica básica': {
+    nombre: 'practica clinica básica',
+    descripcion: '',
+    requisito: null
+       },
+  'practica de campo básica': {
+    nombre: 'practica de campo básica',
+    descripcion: '',
+    requisito: null
+        },
+
+     // 🔸 Semestre 4
+  'bases inmunologicas': {
+    nombre: 'bases inmunologicas',
+    descripcion: '',
+    requisito: 'patologia I', 'bases de tecn. diagnostica'
+      },
+  'fisiologia II': {
+    nombre: 'fisiologia II',
+    descripcion: '',
+    requisito: 'patologia I', 'nutrición'
+      },
+  'famracologia general': {
+    nombre: 'farmacologia general',
+    descripcion: '',
+    requisito: null
+      },
+  'int. producción animal': {
+    nombre: 'int. producción animal',
+    descripcion: '',
+    requisito: 'MACA'
+      },
+  'agentes biologicos y patogenos': {
+    nombre: 'agentes biologicos y patogenos',
+    descripcion: '',
+    requisito: 'enf. infecciosas y parasitarias', 'bases de tecn. diagnosticas'
+      },
+  'bases economicas': {
+    nombre: 'bases economicas',
+    descripcion: '',
+    requisito: 'MAPLAN'
+      },
+  'MAEP': {
+    nombre: 'MAEP',
+    descripcion: '',
+    requisito: null
+      },
+  'modulo int. ciclo basico': {
+    nombre: 'modulo int. ciclo basico',
+    descripcion: '',
+    requisito: null
+        },
+
+ // 🔸 Semestre 5
+  'enf. infecciosas y parasitarias': {
+    nombre: 'enf. infecciosas y parasitarias',
+    descripcion: '',
+    requisito: 'salud pública veterinaria', 'patologia en explotaciones'
+         },
+  'patologia I': {
+    nombre: 'patologia I',
+    descripcion: '',
+    requisito: 'patologia II'
+         },
+  'nutrición': {
+    nombre: 'nutrición',
+    descripcion: '',
+    requisito: 'alimentación'
+         },
+  'bases de tecn. diagnosticas': {
+    nombre: 'bases de tecn. diagnosticas',
+    descripcion: '',
+    requisito: 'metodos de exploracion clinica'
+         },
+  'MAIG': {
+    nombre: 'MAIG',
+    descripcion: '',
+    requisito: 'genética básica'
+         },
+
+      // 🔸 Semestre 6
+  'genética básica': {
+    nombre: 'genética básica',
+    descripcion: '',
+    requisito: 'reproducción', 'biotecnología reproductiva'
+         },
+    'patologia II': {
+    nombre: 'patologia II',
+    descripcion: '',
+    requisito: 'patologia III', 'patologia diagnostica'
+         },
+    'alimentación': {
+    nombre: 'alimentación',
+    descripcion: '',
+    requisito: 'manejos productivos', 'manejos productivos'
+         },
+    'metodos de exploración clinica': {
+    nombre: 'metodos de exploración clinica',
+    descripcion: '',
+    requisito: 'medicina nivel I'
+         },
+    'MAAT': {
+    nombre: 'MAAT',
+    descripcion: '',
+    requisito: ''
+         },
+
+    // 🔸 Semestre 6
+    '': {
+    nombre: '',
+    descripcion: '',
+    requisito: ''
+         },
+    '': {
+    nombre: '',
+    descripcion: '',
+    requisito: ''
+         },
+    '': {
+    nombre: '',
+    descripcion: '',
+    requisito: ''
+         },
+   '': {
+    nombre: '',
+    descripcion: '',
+    requisito: ''
+         },
+   '': {
+    nombre: '',
+    descripcion: '',
+    requisito: ''
+         },
+   '': {
+    nombre: '',
+    descripcion: '',
+    requisito: ''
+         },
+   '': {
+    nombre: '',
+    descripcion: '',
+    requisito: ''
+         },
+   '': {
+    nombre: '',
+    descripcion: '',
+    requisito: ''
+         },
+   '': {
+    nombre: '',
+    descripcion: '',
+    requisito: ''
+         },
+  
+    
 
 function completar(id) {
   const elem = document.querySelector(`[onclick="completar('${id}')"]`);
